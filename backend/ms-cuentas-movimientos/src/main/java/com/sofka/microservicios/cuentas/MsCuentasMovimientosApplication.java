e1f44dbafff5cf43d45a4c6ec1a68dee69436997
Aura Cristina Garzón Rodríguez <auragarzonr@gmail.com>
2026-01-15 21:49:46 -0500

    Initial commit for MsCuentasMovimientosApplication
    This commit introduces the MsCuentasMovimientosApplication class, which serves as the entry point for the Cuentas Movimientos microservice. It sets up the Spring Boot application by using the @SpringBootApplication annotation. The main method is implemented to launch the application, enabling the necessary components and configurations to run within a Spring context. This foundational setup is crucial for further development and feature implementation in the microservice architecture.

---
+++ backend/ms-cuentas-movimientos/src/main/java/com/sofka/microservicios/cuentas/MsCuentasMovimientosApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.microservicios.cuentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCuentasMovimientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCuentasMovimientosApplication.class, args);
+	}
+
+}
