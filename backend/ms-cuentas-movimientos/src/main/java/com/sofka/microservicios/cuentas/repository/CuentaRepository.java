2a8a58edbcb7399c52fadaa3bb3ee9c8b024b113
Aura Cristina Garzón Rodríguez <auragarzonr@gmail.com>
2026-01-16 21:13:54 -0500

    Add CuentaRepository interface for account management
    This commit introduces the CuentaRepository interface, which extends JpaRepository to facilitate data access for the Cuenta entity. It includes methods to retrieve accounts by account number and by client ID. The addition of this repository interface lays the groundwork for implementing account management features within the microservice, enabling efficient interaction with the underlying database for account-related operations.

---
+++ backend/ms-cuentas-movimientos/src/main/java/com/sofka/microservicios/cuentas/repository/CuentaRepository.java
@@ -0,0 +1,15 @@
+package com.sofka.microservicios.cuentas.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sofka.microservicios.cuentas.domain.Cuenta;
+
+@Repository
+public interface CuentaRepository extends JpaRepository<Cuenta, Long> {
+    Optional<Cuenta> findByNumeroCuenta(String numeroCuenta);
+    List<Cuenta> findByClienteId(Long clienteId);
+}
