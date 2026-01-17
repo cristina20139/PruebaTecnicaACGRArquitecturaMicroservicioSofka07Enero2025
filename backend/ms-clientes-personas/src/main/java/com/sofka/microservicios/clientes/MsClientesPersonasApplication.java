b9417352c8fffb0b812f4f021477b60be7ea5950
Aura Cristina Garzón Rodríguez <auragarzonr@gmail.com>
2026-01-16 21:13:42 -0500

    Add logging for Swagger UI URL on application start
    This commit introduces logging functionality to the MsClientesPersonasApplication class. A Logger instance is created using SLF4J, and an event listener is added to the ApplicationReadyEvent. When the application starts and is ready, it logs the URL for the Swagger UI, helping developers easily find and access the API documentation at runtime. Additionally, the code has been cleaned up by removing an unused comment block and organizing the import statements. The application is also annotated with @EnableAsync to facilitate asynchronous processing in the future.

--- backend/ms-clientes-personas/src/main/java/com/sofka/microservicios/clientes/MsClientesPersonasApplication.java
+++ backend/ms-clientes-personas/src/main/java/com/sofka/microservicios/clientes/MsClientesPersonasApplication.java
@@ -1,17 +1,25 @@
 package com.sofka.microservicios.clientes;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-/**
- * 
- */
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class MsClientesPersonasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsClientesPersonasApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(MsClientesPersonasApplication.class);
 
+    public static void main(String[] args) {
+        SpringApplication.run(MsClientesPersonasApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void mostrarSwaggerUrl() {
+        log.info("Swagger UI: http://localhost:8081/swagger-ui/index.html");
+    }
 }
